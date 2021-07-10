dbe69c583f8e442bde93a5fb0c693939974decc3
Christian <christiandr@titamedia.com>
2021-07-10 18:06:49 -0500

    Primer commit de organización de ambiente.
    Se realiza commit con base en Java, para la creación de ejemplo con
    principios SOLID

---
+++ src/solidprinciples/NetflixUser.java
@@ -0,0 +1,20 @@
+package solidprinciples;
+
+public class NetflixUser {   
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static void userLogin(
+        int userID, 
+        String nickName, 
+        String password, 
+        String email
+    ) {
+        System.out.println(userID);
+        System.out.println(nickName);
+        System.out.println(password);
+        System.out.println(email);
+    }
+}
---
+++ src/solidprinciples/SolidPrinciples.java
@@ -0,0 +1,10 @@
+package solidprinciples;
+
+public class SolidPrinciples {
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NetflixUser.userLogin(25, "Christian", "hello", "1234");
+    }
+    
+}
